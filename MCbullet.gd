49a863fa50fb536b50c1b575bb9fa0da8ea9e641
Marlom <cianstentoft@gmail.com>
2022-12-02 12:45:34 +0100

    tried to make shoot the bullet but could not

---
+++ MCbullet.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+var velocity = Vector2 (0, -1)
+var speed = 300
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	var collision_info = move_and_collide(velocity.normalized() * delta * speed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
